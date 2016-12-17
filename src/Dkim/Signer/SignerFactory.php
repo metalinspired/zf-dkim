55d8787ddbaa87fc5e395979d58b0973a66caa8a
Giovanni Iembo <giovanni.iembo@gmail.com>
2016-12-17 17:12:24 +0700

    Fixes

--- src/Dkim/Signer/SignerFactory.php
+++ src/Dkim/Signer/SignerFactory.php
@@ -5,13 +5,16 @@ namespace Dkim\Signer;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-class SignerFactory implements FactoryInterface
-{
+class SignerFactory implements FactoryInterface {
 
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
-        $signer = new Signer($serviceLocator->get('Config'));
-        return $signer;
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+
+        return new Signer($serviceLocator->get('Config'));
+    }
+
+    public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = null) {
+
+        return new Signer($container->get('Config'));
     }
 
 }
